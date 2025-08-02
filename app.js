a9124be251e25acb27925d2eb5ec011d1c95fb1b
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-08-02 23:57:45 +0530

    CREATE ROUTE

--- app.js
+++ app.js
@@ -21,17 +21,42 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended : true}));
 
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-// INDEX ROUTE
+// -------------------INDEX ROUTE-------------------
 app.get("/listings",async (req, res) => {
    const allListings = await Listing.find({});
    res.render("listings/index.ejs", {allListings});
 });
+
+// -------------------NEW ROUTE--------------------
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+// -------------------SHOW ROUTE-------------------
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    console.log("Fetched listing:", listing); 
+    res.render("listings/show.ejs", {listing});
+});
+
+// -------------------CREATE ROUTE-------------------
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+
+
+
 
 // app.get("/testListening", async (req, res) => {
 //     let sampleListing = new Listing ({
