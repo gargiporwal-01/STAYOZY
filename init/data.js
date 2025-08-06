3797cd93ef700b283c4fe4b5d814e594ce8a4f17
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-08-06 20:45:44 +0530

    Listing Your Space

--- init/data.js
+++ init/data.js
@@ -5,7 +5,7 @@ const sampleListings = [
         "Wake up to the sound of waves and enjoy uninterrupted ocean views from this serene coastal cottage.",
       image: {
         filename: "listingimage",
-        url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        url: "https://images.unsplash.com/photo-1621090162511-006cd5d9d6b5?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       },
       price: 1800,
       location: "Santa Barbara",
