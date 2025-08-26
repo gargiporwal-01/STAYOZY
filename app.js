86e095d877f4726f7663db6978085a672d38b671
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-08-26 20:57:28 +0530

    Middleware

--- app.js
+++ app.js
@@ -35,6 +35,16 @@ app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400, errMsg);
+    } else {
+      next();
+    }
+};
+
 // -------------------INDEX ROUTE-------------------
 app.get("/listings", wrapAsync (async (req, res) => {
    const allListings = await Listing.find({});
@@ -59,12 +69,8 @@ app.get(
 // -------------------CREATE ROUTE-------------------
 app.post(
     "/listings",
+    validateListing,
     wrapAsync (async (req, res, next) => {
-     let result = listingSchema.validate(req.body);
-     console.log(result);
-       if (result.error) {
-          throw new ExpressError(400, result.error);
-       }
      const newListing = new Listing(req.body.listing);
      await newListing.save();
      res.redirect("/listings");
@@ -84,10 +90,8 @@ app.get(
 // -------------------UPDATE ROUTE-------------------
 app.put(
     "/listings/:id", 
+    validateListing,
     wrapAsync (async (req, res) => {
-    if(!req.body.listing) {
-        throw new ExpressError(400, "Send valid data for listing");
-    }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
