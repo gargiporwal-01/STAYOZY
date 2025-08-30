2d1c82fadb273be485ea010789f4318e281395db
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-08-30 18:33:21 +0530

    Render Review

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const  { listingSchema } = require("./schema.js");
+const  { listingSchema , reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/stayozy";
@@ -46,6 +46,16 @@ const validateListing = (req, res, next) => {
     }
 };
 
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400, errMsg);
+    } else {
+      next();
+    }
+};
+
 // -------------------INDEX ROUTE-------------------
 app.get("/listings", wrapAsync (async (req, res) => {
    const allListings = await Listing.find({});
@@ -62,8 +72,7 @@ app.get(
     "/listings/:id", 
     wrapAsync (async (req, res) => {
     let {id} = req.params;
-    const listing = await Listing.findById(id);
-    console.log("Fetched listing:", listing); 
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", {listing});
 }));
 
@@ -109,7 +118,7 @@ app.delete(
 }));
 
 // -------------------REVIEWS ROUTE(Post Route)-------------------
-app.post("/listings/:id/reviews", async (req, res) => {
+app.post("/listings/:id/reviews", validateReview, wrapAsync (async (req, res) => {
   let listing = await Listing.findById(req.params.id);
   let newReview = new Review(req.body.review);
 
@@ -119,7 +128,7 @@ app.post("/listings/:id/reviews", async (req, res) => {
   await listing.save();
 
   res.redirect(`/listings/${listing._id}`);
-});
+}));
 
 
 
--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-module.exports.listingSchema = Joi.object({
+const listingSchema = Joi.object({
     listing : Joi.object({
         title : Joi.string().required(),
         description : Joi.string().required(),
@@ -10,6 +10,15 @@ module.exports.listingSchema = Joi.object({
         image: Joi.object({
             url: Joi.string().uri().allow("", null),
             filename: Joi.string().allow("", null)
-          }).optional()
+        }).optional()
     }).required(),
 });
+
+const reviewSchema = Joi.object({
+    review : Joi.object({
+        rating : Joi.number().required().min(1).max(5), 
+        comment : Joi.string().required(),
+    }).required()
+});
+
+module.exports = { listingSchema, reviewSchema };
