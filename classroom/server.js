82848fe3fc89e1a3fb919edea8653e46e4d9620d
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-09-01 23:15:00 +0530

    Connect flash

--- classroom/server.js
+++ classroom/server.js
@@ -1,19 +1,45 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
+const users = require("./routes/user.js");
+const posts = require("./routes/post.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
-app.use(cookieParser());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/getcookies", (req, res) => {
-    res.cookie("greet", "namaste");
-    res.cookie("madeIn", "India");
-    res.send("sent you some cookies!!");
+const sessionOptions = {
+    secret : "mysupersecreystring",
+    resave : false,
+    saveUninitialized : true,
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.get("/register", (req, res) => {
+    let { name = "anonymous"} = req.query;
+    req.session.name = name;
+    req.flash("success", "user registered successfully");
+    res.redirect("/hello");
 });
 
-app.get("/", (req, res) => {
-    console.dir(req.cookies);
-    res.send("sent you some cookies");
+app.get("/hello", (req, res) => {
+    res.send(`hello, ${req.session.name}`);
 });
+
+// app.get("/reqcount", (req,res) => {
+//     if (req.session.count) {
+//         req.session.count++;
+//     } else {
+//         req.session.count = 1;
+//     }
+//     res.send(`You sent a request ${req.session.count}  times`);
+// });
+
+// app.get("/test", (req, res) => {
+//     res.send("test succesful");
+// });
 
 app.listen(3000, () => {
     console.log("server is listening at 3000");
