360327c13032dd263a0ab98d893b2680ebe47f04
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-09-06 16:40:46 +0530

    Setting Authorization

--- routes/listing.js
+++ routes/listing.js
@@ -25,7 +25,12 @@ router.get(
     wrapAsync (async (req, res) => {
      let {id} = req.params;
      const listing = await Listing.findById(id)
-      .populate("reviews")
+      .populate({
+        path : "reviews",
+        populate : {
+            path : "author",
+        },
+      })
       .populate("owner");
      if(!listing) {
         req.flash("error", "The requested listing could not be found");
--- routes/review.js
+++ routes/review.js
@@ -4,7 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
 
 
@@ -13,12 +13,13 @@ const { validateReview } = require("../middleware.js");
 // --------------------POST REVIEW ROUTE-------------------
 router.post(
   "/", 
+  isLoggedIn,
   validateReview, 
   wrapAsync (async (req, res) => {
 
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-  
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
   
     await newReview.save();
@@ -30,6 +31,8 @@ router.post(
   // --------------------DELETE REVIEW ROUTE-------------------
   router.delete(
     "/:reviewId",
+    isLoggedIn,
+    isReviewAuthor,
     wrapAsync(async (req, res) => {
       let {id, reviewId } = req.params;
   
