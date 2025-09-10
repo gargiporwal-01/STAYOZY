bff1455cf998f2d064f03f7309b0f8768e677236
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-09-10 21:43:35 +0530

    Stayozy - Travel Stay Platform

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL || process.env.MONGO_URL || "mongodb://127.0.0.1:27017/stayozy";
 
 main()
   .then(() => {
@@ -41,21 +41,28 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = MongoStore.create({
-  mongoUrl : dbUrl,
-  crypto : {
-    secret : process.env.SECRET,
-  },
-  touchAfter : 24 * 3600,
-});
+let store;
+try {
+  store = MongoStore.create({
+    mongoUrl : dbUrl,
+    crypto : {
+      secret : process.env.SECRET || "mysupersecretcode",
+    },
+    touchAfter : 24 * 3600,
+  });
+} catch (err) {
+  console.error("Failed to initialize Mongo session store", err);
+}
 
-store.on("error", () => {
-  console.log("ERROR in MONGO SESSION STORE", err);
-});
+if (store) {
+  store.on("error", (err) => {
+    console.log("ERROR in MONGO SESSION STORE", err);
+  });
+}
 
 const sessionOptions = {
-  store,
-  secret : process.env.SECRET,
+  ...(store ? { store } : {}),
+  secret : process.env.SECRET || "mysupersecretcode",
   resave : false,
   saveUninitialized : true,
   cookie : {
